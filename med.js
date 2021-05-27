51961df13db40f717bc1de67f634dd2fc2c9bb73
Denys Tsybko <72654542+edexmusic@users.noreply.github.com>
2021-05-27 18:47:45 +0300

    Add files via upload

--- med.js
+++ med.js
@@ -1,9 +1,18 @@
 (function() {
     const header = document.querySelector('.header');
+    const  line = document.querySelector('.line');
     window.onscroll = () => {
         if (window.pageYOffset > 40) {
+            line.classList.add('moved'),
             header.classList.add('header_moved')
         } else
+        line.classList.remove('moved'),
         header.classList.remove('header_moved')
     }
   }());
+
+    const nav = document.querySelector('.nav');
+    document.querySelector('.hamburger-menu').onclick = function() {
+        this.classList.toggle('active-one');
+        nav.classList.toggle('active');
+    }
